7ae24f20ba9f70e82df8f071f33431eee3500b9f
Saarthak.Gupta <saarthak.gupta@HsAdmins-MacBook-Pro-2.local>
2022-12-23 15:21:30 +0530

    test

--- src/main/java/org/example/TestDemo.java
+++ src/main/java/org/example/TestDemo.java
@@ -1,5 +1,16 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class TestDemo {
     private String name;
 
