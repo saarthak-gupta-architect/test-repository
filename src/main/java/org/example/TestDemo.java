4a019a18d2f11b255bbfb053c65e64736e508f55
Saarthak.Gupta <saarthak.gupta@HsAdmins-MacBook-Pro-2.local>
2022-12-23 15:20:08 +0530

    test

--- src/main/java/org/example/TestDemo.java
+++ src/main/java/org/example/TestDemo.java
@@ -5,29 +5,4 @@ public class TestDemo {
 
     private String phoneNumber;
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(final String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public TestDemo(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "TestDemo{" + "name='" + name + '\'' + ", phoneNumber='" + phoneNumber + '\'' + '}';
-    }
 }
