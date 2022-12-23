72a3e21289274c6d0d7061e44f49ccb6217972a3
Saarthak.Gupta <saarthak.gupta@HsAdmins-MacBook-Pro-2.local>
2022-12-23 14:22:21 +0530

    init

---
+++ src/main/java/org/example/TestDemo.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class TestDemo {
+    private String name;
+
+    public TestDemo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
